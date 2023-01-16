e3f57aa9fb790250e5ba5692885fba5a353a6a3d
Nathaniel Gomez-Han <nathaniel.gomez.han@gmail.com>
2023-01-16 14:26:40 -0600

    wdv341 homepage and git terms

---
+++ wdv341.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <!--
+            Author: Nathaniel Han
+            Date: January 16 2023
+        -->
+        <meta http-equiv="X-UA-Compatible" content ="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Nathaniel's WDV341 Homework Page</title>
+    </head>
+    <body>
+        <img src="/img/nthan_logo_white.png" height="150px" alt="Nathaniel Han's Logo"/>
+		<p>
+			<a href="/">&lt; Back to Home</a>
+		</p>
+		<h1>WDV 341 Homework Links</h1>
+		<h2>Assignments:</h2>
+		<ul>
+			<li><a href="unit-2/git-terms.html">2-1: Download Git Client & Create a Repository Account (Research and Define Git Terms)</a></li>
+		</ul>
+		
+		<h2>Skill Tests:</h2>
+		<ul>
+
+		</ul>
+</html>
