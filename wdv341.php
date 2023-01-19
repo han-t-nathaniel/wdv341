3e34b86ae0571c3f134459d38ceb793d9cca183a
nathaniel-gomez-han <nathaniel.gomez.han@gmail.com>
2023-01-19 09:39:57 -0600

    Structure/style update. Fixed body tag.

--- wdv341.php
+++ wdv341.php
@@ -4,25 +4,43 @@
         <meta charset="UTF-8">
         <!--
             Author: Nathaniel Han
-            Date: January 16 2023
+            Date: January 19 2023
         -->
         <meta http-equiv="X-UA-Compatible" content ="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Nathaniel's WDV341 Homework Page</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Libre+Baskerville:wght@400;700&display=swap" rel="stylesheet">        
+        <style>
+            body {
+                margin: 0;
+            }
+
+            h1, h2, h3, h4, h5, h6 {
+                font-family: 'Libre Baskerville', serif;
+            }
+            
+            nav, main {
+                padding: 0 200px;
+            }
+            nav {
+                height: 80px;
+                line-height: 80px;
+                background: rgb(218, 218, 224);
+            }
+        </style>
     </head>
     <body>
-        <img src="/img/nthan_logo_white.png" height="150px" alt="Nathaniel Han's Logo"/>
-		<p>
+		<nav>
 			<a href="/">&lt; Back to Home</a>
-		</p>
-		<h1>WDV 341 Homework Links</h1>
-		<h2>Assignments:</h2>
-		<ul>
-			<li><a href="unit-2/git-terms.html">2-1: Download Git Client & Create a Repository Account (Research and Define Git Terms)</a></li>
-		</ul>
-		
-		<h2>Skill Tests:</h2>
-		<ul>
-
-		</ul>
+        </nav>
+        <main>
+            <h1>WDV 341 Homework Links</h1>
+            <h2>Assignments:</h2>
+            <ul>
+            <li><a href="unit-2/git-terms.html">2-1: Download Git Client & Create a Repository Account (Research and Define Git Terms)</a></li>
+            </ul>
+        </main>
+    </body>
 </html>
