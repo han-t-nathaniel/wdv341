25b16cd7d3f505719afe6c8b436ca20f178e05ce
nathaniel-gomez-han <nathaniel.gomez.han@gmail.com>
2023-02-02 17:22:31 -0600

    5-1: HTML Form Processor

---
+++ unit-5/DemonstrateFormHandler.php
@@ -0,0 +1,91 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+	// Get form input.
+	$firstName = $_POST["firstName"];
+	$lastName = $_POST["lastName"];
+	$email = $_POST["email"];
+	$academicStanding = $_POST["academicStanding"];
+	$selectedMajor = $_POST["selectedMajor"];
+	$comments = $_POST["comments"];
+
+	// Find out which checkboxes were selected.
+	$sendProgramInfo = array_key_exists("sendProgramInfo", $_POST) ? true : false;
+	$contactAdvisor = array_key_exists("contactAdvisor", $_POST) ? true : false;
+
+	$programAcronyms = [
+		"cis" => "Computer Information Systems",
+		"grd" => "Graphic Design",
+		"wdv" => "Web Development"
+	];
+
+	$programPhrase = $programAcronyms[$selectedMajor];
+
+	$confirmationMessage = 
+		"<p>Dear $firstName,</p>
+		<p>Thank you for your interest in DMACC.</p>
+		<p>We have you listed as a $academicStanding starting this fall.</p>
+		<p>You have declared $programPhrase as your major.</p>
+		<p>Based on your responses we will provide the following information in our confirmation email to you at <em>$email</em>.</p>
+		<p><input type=\"checkbox\" id=\"sendProgramInfo\" disabled"; 
+		
+	if ($sendProgramInfo) {
+		$confirmationMessage .= " checked";
+	}
+
+	$confirmationMessage .=
+		"/>
+		<label for=\"contactAdvisor\">Please contact me with program information</label><br/>
+		<input type=\"checkbox\" id=\"contactAdvisor\" disabled"; 
+		
+		if ($contactAdvisor) {
+			$confirmationMessage .= " checked";
+		}
+	
+		$confirmationMessage .=
+			"/>
+			<label for=\"contactAdvisor\">I would like to contact a program advisor</label></p>
+			<p>You have shared the following comments which we will review:</p>
+			<blockquote><em>$comments<em></blockquote>";
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <!--
+      Author: Nathaniel Han
+      Date: February 2 2023
+  -->
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>WDV 341 5-1: HTML Form Processor | Confirmation Page</title>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Jost&family=Libre+Baskerville:wght@400;700&display=swap"
+    rel="stylesheet">
+  <link rel="stylesheet" href="/css/bubbles.css">
+</head>
+
+<body>
+  <div class="body-bg"></div>
+  <nav>
+    <div>
+      <a href="/">
+        <object class="site-logo" type="image/svg+xml" data="/img/ngh-logo-white.svg"></object>
+      </a>
+    </div>
+    <div>
+      <a href="/">&lt; Back to Home</a>
+    </div>
+  </nav>
+  <main>
+    <h1>WDV 341 | 5-1: HTML Form Processor</h1>
+	<h2>Confirmation Page</h2>
+    <p>
+		<?= $confirmationMessage ?>
+	</p>
+  </main>
+</body>
+
+</html>
--- unit-5/inputProcess.php
+++ unit-5/inputProcess.php
@@ -1,18 +1,28 @@
+<?php
+$firstName = $_POST['firstName'];
+$lastName = $_POST['lastName'];
+
+echo "First Name: $firstName";
+echo "Last Name: $lastName";
+?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <meta charset="UTF-8">
+        <meta charset="UTF-8"></meta>
         <!--
             Author: Nathaniel Han
-            Date: January 21 2023
+            Date: January 31 2023
         -->
         <meta http-equiv="X-UA-Compatible" content ="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Nathaniel's WDV341 Homework Page</title>
+        <title>4-1: PHP Functions</title>
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Jost&family=Libre+Baskerville:wght@400;700&display=swap" rel="stylesheet">     
+        <link href="https://fonts.googleapis.com/css2?family=Jost&family=Libre+Baskerville:wght@400;700&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="/css/bubbles.css">
+        <script>
+            let languages = <?php echo $languagesArrayJS; ?>
+        </script>
     </head>
     <body>
         <div class="body-bg"></div>
@@ -23,18 +33,15 @@
                 </a>
             </div>
             <div>
-			    <a href="/">&lt; Back to Home</a>
+                <a href="../wdv341.php">&lt; Back</a>
             </div>
         </nav>
         <main>
-            <h1>WDV 341 Homework Links</h1>
-            <h2>Assignments:</h2>
-            <ul>
-            <li><a href="unit-2/git-terms.html">2-1: Download Git Client & Create a Repository Account (Research and Define Git Terms)</a></li>
-            <li><a href="unit-3/php-basics.php">3-1: PHP Basics</a></li>
-            <li><a href="unit-4/php-functions.php">4-1: PHP Functions</a></li>
-            <li><a href="unit-5/WDV101-DemonstrateInputForm.html">5-1: HTML Form Processor</a></li>
-            </ul>
+            <h1>Your form has been processed on the server!</h1>
+            <h2>Confirmation Page!</h2>
+            <p>You entered the following information:</p>
+            <p>First Name: <?php echo $firstName ?></p>
+            <p>Last Name: <?php echo $lastName ?></p>
         </main>
     </body>
 </html>
